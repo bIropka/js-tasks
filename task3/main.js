2ff2702a0b6520d9a53b14cc869992bd7bc14a62
Perimeter <Perimeter1989@gmail.com>
2018-11-01 23:10:45 +0200

    3 and 5

--- task3/main.js
+++ task3/main.js
@@ -1,26 +1,26 @@
 function maxDivisionNumbers(n) {
-    var arr = [];
-    for(var k = 2; k <= n; k++){
-        arr[k] = true;
+    var num = Math.trunc(Math.sqrt(n)),
+        arr = [],
+        result = 0;
+    for(var k = 2; k <= num; k++){
+        arr[k] = k;
     }
-    for(k = 2; k*k <= n; k++){
+    for(k = 2; k*k <= num; k++){
         if(arr[k] === k){
-            for( var j=k*k; j<=n; j+=k){
-                arr[j] = false;
+            for( var j=k*k; j<= num; j+=k){
+                delete arr[j];
             }
         }
     }
-/*    var d = 0;
-    for(var t = n; t >= 1; t--) {
+    for(var t = num; t >= 1; t--) {
         if(arr[t] >= 1 && n % arr[t] === 0){
-            d = arr[t];
+            result = arr[t];
             break;
         }
     }
-    return d;*/
-    return arr;
+    return result;
 }
-console.log(maxDivisionNumbers(27));
+console.log(maxDivisionNumbers(600851475143));
 
 // 13195
 //600851475143
--- task5/main.js
+++ task5/main.js
@@ -1,17 +1,26 @@
 function smallestNumber(n,k) {
-    if(n == 1){
-        n++;
+    var arr = [],
+        result = 1;
+    for(var j = 2; j <= k; j++){
+        arr[j] = j;
     }
-    var d=2;
-    // перебираем возможные делители от 2 до sqrt(n)
-    while(n <= k){
-        for(; d<=n; d++){
-
-            if(n % d === 0){
-                console.log(d);
+    for(j = 2; j <= k; j++){
+        if(arr[j] === j){
+            for( var c=j+j; c <= k; c+=j){
+                if( arr[c] % (j * j) === 0){
+                    arr[c] = j;
+                }else{
+                    delete arr[c];
+                }
             }
         }
-        n++;
     }
+    arr.forEach(function(item) {
+        result *= item;
+    });
+    return result;
 }
-console.log(smallestNumber(2,10));
+console.log(smallestNumber(1,20));
+
+
+
