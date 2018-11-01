7f310178f09df8f01401e1b0e05a0c8c829aa4c2
Perimeter <Perimeter1989@gmail.com>
2018-11-01 09:43:15 +0200

    333

--- task3/main.js
+++ task3/main.js
@@ -1,36 +1,62 @@
-/*function maxDivisionNumbers(n) {
+function maxDivisionNumbers(n) {
+    var arr = [1];
+    for(var k = 2; k < n; k++){
 
-    var myArr = [];
-    while ( i > 1 ) {
-        if (n % i === 0) {
-            if(i % 2 !== 0 && i % 3 !== 0 && i % 5 !== 0 && i % 7 !== 0){
-                myArr.push(i);
-                break;
+        arr.forEach(function(item){
+            if( k % item === 0){
+                console.log(k);
+                arr.push(k);
             }
-        }
-        i--;
+        });
     }
-    return myArr;
-}*/
-function isprime(n){
-    var result = 0;
-    for(var i = 7; i < n; i++){
+    return arr;
+}
+console.log(maxDivisionNumbers(27));
+/*var arr = [0];
+arr.forEach(function(item) {
+    j = 2 / item;
+    arr.push(j);
+});
+console.log(arr);*/
+
+/*function isprime(n){
+    for(var i = 2; i < n; i++){
         if(n%i === 0){
-            for( var d = 7; d*d < i; d++){
+            for( var d = 2; d*d < i; d++){
                 if(i%d === 0){
-                    console.log(i);
+                    console.log(n/i);
+                    // return(n/i);
                 }
             }
         }
-
     }
-    return result;
-}
-console.log(isprime(13195));
-//maxDivisionNumbers();
+}*/
+/*console.log(isprime(13195));*/
+
 
 // console.log(maxDivisionNumbers(13195));
 
 //377
 // 13195
 //600851475143
+//10086647
+
+/*
+function sieve(n){
+    var S = [];
+    for( var k=2; k<=n; k++){
+        S[k]=1;
+    }
+    for( var k=2; k*k<=n; k++){
+        // если k - простое (не вычеркнуто)
+        if(S[k]==1){
+            // то вычеркнем кратные k
+            for(l=k*k; l<=n; l+=k){
+                S[l]=0;
+            }
+        }
+    }
+    return S;
+}
+
+console.log(sieve(13195));*/
